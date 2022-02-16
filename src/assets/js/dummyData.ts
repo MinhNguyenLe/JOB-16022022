3063c7bb5832ad5d622606609c6e508591f8bd6b
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2022-02-16 10:44:59 +0700

    code base

---
+++ src/assets/js/dummyData.ts
@@ -0,0 +1,62 @@
+export const dummyData = [
+  {
+    name: "Hugo Vinson",
+    image:
+      "https://i.picsum.photos/id/1002/367/267.jpg?hmac=yLqY6breRyPZ3GcA3nJGtjgvCeRbKQZTFx8WC29OkZU",
+  },
+  {
+    name: "Katie Mckay",
+    image:
+      "https://i.picsum.photos/id/1003/367/267.jpg?hmac=1VHS13exKZ5QWJdtDTu0iCUTBZKCE_clSpR-wc3PW3g",
+  },
+  {
+    name: "Ianis Shah",
+    image:
+      "https://i.picsum.photos/id/1004/367/267.jpg?hmac=dNFSbCRe8enn3ikNS0FvmnagW4IzORvJuuTAJ_UUGf8",
+  },
+  {
+    name: "Lyla-Rose Friedman",
+    image:
+      "https://i.picsum.photos/id/1005/367/267.jpg?hmac=bl_eyI1wwd6n-Q120mDottBNmCDNBurz7Z-b5IOeJU0",
+  },
+  {
+    name: "Ryan Raymond",
+    image:
+      "https://i.picsum.photos/id/0/367/267.jpg?hmac=7vvdjNHaShwe3Ul3DAaQIBBz0RnNLfUcEaCV-94NNLE",
+  },
+  {
+    name: "Coen Jenkins",
+    image:
+      "https://i.picsum.photos/id/1/367/267.jpg?hmac=jZdc5TviQPVhxLyvyU8siO-I5FMVXVZpBhsBYKbBJpM",
+  },
+  {
+    name: "John Wick",
+    image:
+      "https://i.picsum.photos/id/10/367/267.jpg?hmac=XRdepQX9y39tepelazZaEAxb6SbCWtual9_w28FPb6U",
+  },
+  {
+    name: "Nancy Ace",
+    image:
+      "https://i.picsum.photos/id/100/367/267.jpg?hmac=mNdSdA1Zh6w4qessdp5n207IFw3q_8FbbQ1gIr0jYBs",
+  },
+  {
+    name: "Alicia Willams",
+    image:
+      "https://i.picsum.photos/id/102/4320/3240.jpg?hmac=ico2KysoswVG8E8r550V_afIWN963F6ygTVrqHeHeRc",
+  },
+  {
+    name: "Mila Azul",
+    image:
+      "https://i.picsum.photos/id/1022/367/267.jpg?hmac=0mbNHdQLUp_d5Cat6xNrL06S_07ikrdfLb6FDKJuir4",
+  },
+  {
+    name: "Katya Clover",
+    image:
+      "https://i.picsum.photos/id/1021/367/267.jpg?hmac=Com4ty_AQAGdXDh2E-s8hBqQ0ZgtB519_eVnh4Z5slY",
+  },
+  {
+    name: "Miyuki Arisaka",
+    image:
+      "https://i.picsum.photos/id/1024/367/267.jpg?hmac=pOq95j6N3CkJ1yXDdkh3xMr03DalA77-7v1aAyp4nWE",
+  },
+];
